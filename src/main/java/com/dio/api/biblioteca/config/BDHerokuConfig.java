febecec0669aa12b7bdf0eb1d250862e8d141f20
Jaques Oliveira <jaques.oliveira@gmail.com>
2021-09-21 12:33:42 -0300

    datasource

--- src/main/java/com/dio/api/biblioteca/config/BDHerokuConfig.java
+++ src/main/java/com/dio/api/biblioteca/config/BDHerokuConfig.java
@@ -1,2 +1,32 @@
-package com.dio.api.biblioteca.config;public class BDHerokuConfig {
+package com.dio.api.biblioteca.config;
+
+import conn.HerokuPostgresConnection;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Getter
+@Configuration
+@ConfigurationProperties("spring.datasource")
+public class BDHerokuConfig {
+
+    private String url;
+    private String driverClassName;
+    private String userName;
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(HerokuPostgresConnection.DRIVER_CLASS_NAME);
+        dataSourceBuilder.url(HerokuPostgresConnection.URL);
+        dataSourceBuilder.username(HerokuPostgresConnection.USER_NAME);
+        dataSourceBuilder.password(HerokuPostgresConnection.PASSWORD);
+        return dataSourceBuilder.build();
+    }
 }
